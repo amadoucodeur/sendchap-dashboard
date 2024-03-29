6a1af2fdf6f4d81659f10df5201ebb598cb85f5c
amadoucodeur <amadoucodeur@gmail.com>
2024-03-29 16:01:36 +0000

    realisation de diagramecard

---
+++ components/DiagrameCard.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function DiagrameCard() {
+  return (
+    <div>
+      <div className="">r</div>
+      <div className="">
+        <p>Envoi d’argent inter réseau</p>
+        <p>Transaction de la journée</p>
+        <p>23450551.6 F CFA</p>
+      </div>
+      <div className="">
+        
+      </div>
+    </div>
+  );
+}
